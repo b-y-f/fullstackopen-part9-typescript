791c6c0c9c5c629b98247d85362879f239b96805
yifan <bbbbb8bbbbb@gmail.com>
2021-11-21 00:12:15 +1300

    9.5 WebBMI ok

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,6 +1,6 @@
 type Result = string
 
-const calculateBmi = (h:number , w: number) : Result => {
+export const calculateBmi = (h:number , w: number) : Result => {
 
     const value = w / (h*0.01)**2
     if (value < 18.5) {
--- index.ts
+++ index.ts
@@ -1,11 +1,28 @@
 const express = require('express');
 const app = express();
 
+import {calculateBmi} from './bmiCalculator'
+
 app.get('/', (_req: any, res: any ) => {
   res.send('Hello Full Stack!');
 });
 
-const PORT = 3003;
+app.get('/bmi', (req:any , res:any) => {
+  const {height, weight} = req.query
+  
+  const bmi = calculateBmi(height, weight)
+
+  if (!height || !weight) {
+    res.json({
+      error: "malformatted parameters"
+    })
+  }else{
+    res.json({height , weight, bmi})
+  }
+  
+})
+
+const PORT = 3002;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
